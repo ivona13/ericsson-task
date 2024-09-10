7b39e3b159b9814386442e2a16c56a2c282a9c1d
Ivona Raguz <ivona.raguz13@gmail.com>
2024-09-10 14:25:48 +0200

    Remove lombok experimental

--- src/main/java/eu/ericsson/task/controller/RaceController.java
+++ src/main/java/eu/ericsson/task/controller/RaceController.java
@@ -7,7 +7,6 @@ import eu.ericsson.task.service.RaceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,22 +18,21 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import static eu.ericsson.task.service.RaceService.validateHarryKart;
-import static lombok.AccessLevel.PRIVATE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@FieldDefaults(level = PRIVATE)
 @RequestMapping("/api/v1")
 @Tag(name = "race", description = "Operations related to race")
 public class RaceController {
 
-    final RaceService raceService;
     @Value("${application.race.number-of-winners}")
     private int numberOfWinners;
 
+    private final RaceService raceService;
+    
     @Operation(summary = "Calculates winners for HarryKart",
             description = "Calculates first three places for HarryKart based on the input XML file")
     @PostMapping(path = "/calculate-winner", consumes = APPLICATION_XML_VALUE, produces = APPLICATION_JSON_VALUE)
